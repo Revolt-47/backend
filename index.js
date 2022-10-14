646bded72256ce8ca637f1da7ab00c69bee32100
Revolt_47 <i201806@nu.edu.pk>
2022-10-14 18:10:25 +0500

    Fully working get and post functions

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json())
 
-app.get("/api",(req,res)=>{
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+app.get("/api",(req,res , next)=>{
     res.json({"users":["userone","usertwo","userthree"]})
 })
 
 
-app.post("/api" , (req , res)=>{
+app.post("/api" , (req , res , next)=>{
     console.log(req.body)
    let name = req.body.name ;
 //    let pass = req.body.pass;
 console.log(name);
-res.json({name:name})
+res.send({name:name})
 
 })
 
